fb1687aaa23ca31d1901927689e11fe6fa1066d4
marcelinol <marcelinolucianom@gmail.com>
2016-10-22 13:09:35 -0200

    Create partials for meditation session room

--- db/schema.rb
+++ db/schema.rb
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20161022134659) do
   create_table "meditation_sessions", force: :cascade do |t|
     t.integer  "duration"
     t.string   "share_link"
-    t.integer  "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.integer  "status",     default: 0
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "participants", force: :cascade do |t|
