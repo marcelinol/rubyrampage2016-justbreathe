9f1cc80f5f697a60d45c56d6872951a425453abe
Diogo Busanello <diogo.busanello@resultadosdigitais.com.br>
2016-10-22 15:20:01 -0200

    Fix Rubocop 'Space inside the bracket'

--- app/models/meditation_session.rb
+++ app/models/meditation_session.rb
@@ -2,7 +2,7 @@ class MeditationSession < ApplicationRecord
   has_many :participants
   accepts_nested_attributes_for :participants
 
-  enum status: [ :waiting, :started, :finished ]
+  enum status: [:waiting, :started, :finished]
 
   validates :duration, presence: true, numericality: { greater_than: 0 }
 end
