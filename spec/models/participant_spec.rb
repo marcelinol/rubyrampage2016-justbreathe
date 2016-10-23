621f42b569660c65556890e0777f350bbaa9b066
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2016-10-23 18:23:36 -0200

    Add specs for participant

--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Participant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to belong_to(:meditation_session) }
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_length_of(:name).is_at_most(25) }
 end
