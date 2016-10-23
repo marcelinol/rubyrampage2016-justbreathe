15e492014ce48f25a22b1e0d0e3fbad0a3ac9bff
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2016-10-23 18:21:22 -0200

    Add specs for meditation session

--- spec/models/meditation_session_spec.rb
+++ spec/models/meditation_session_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe MeditationSession, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_many(:participants) }
+  it { is_expected.to validate_presence_of(:duration) }
+  it { is_expected.to validate_numericality_of(:duration).is_greater_than(0) }
+  it { is_expected.to accept_nested_attributes_for(:participants) }
 end
