51b4068b817edaad518182d1fea0bc34c194709d
marcelinol <marcelinolucianom@gmail.com>
2017-04-02 17:44:41 -0300

    Update the ready participants. What is missing? - Update participants ready state on database when they click on the screen - Fix the bold participants when the JS updates them (you will see on the screen). it is because of the <label> tag

--- app/assets/javascripts/ready_participants.js
+++ app/assets/javascripts/ready_participants.js
@@ -4,8 +4,14 @@
   $(document).on('updateSessionInfo', function(event, sessionData) { updateSessionInfo(sessionData) });
 
   var updateSessionInfo = function(sessionData) {
-    var participantsNames = parseParticipantsNames(sessionData.participants);
-    updateParticipantsNames(participantsNames);
+    var knownParticipantsLength = $('#js-participants-ready-state').children().length;
+
+    if (knownParticipantsLength != sessionData.participants.length) {
+      var participantsNames = parseParticipantsNames(sessionData.participants);
+      updateParticipantsNames(participantsNames);
+
+      updateParticipantsReadyStates(sessionData.participants);
+    }
   }
 
   var updateParticipantsNames = function(participantsNames) {
@@ -19,4 +25,44 @@
     }
     return participantsNames;
   }
+
+  var updateParticipantsReadyStates = function(participants) {
+    removeOldParticipants();
+    addNewParticipants(participants);
+    return;
+  }
+
+  var removeOldParticipants = function() {
+    var myNode = document.getElementById("js-participants-ready-state");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+  }
+
+  var addNewParticipants = function(participants) {
+    for (var participant of participants) {
+      var checkbox = createCheckbox(participant);
+      document.getElementById('js-participants-ready-state').appendChild(checkbox);
+    }
+    return;
+  }
+
+  var createCheckbox = function(participant) {
+    var div = $("<div>", {"class": "input-group"});
+    var inputId = "js-participant-id-" + participant.id
+    var input = $('<input>', { 'id': inputId, 'data-participant-id': participant.id, 'type': 'checkbox', 'disabled': 'true' });
+
+    if (participant.ready == 'true')
+      input.prop('checked', 'true')
+
+    var label = document.createElement('label')
+    label.htmlFor = "js-participant-id-" + participant.id;
+    label.appendChild(document.createTextNode(participant.name));
+
+    div[0].appendChild(input[0]);
+    div[0].appendChild(label);
+
+    return div[0];
+  }
+
 })(jQuery);
